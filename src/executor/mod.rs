28700619745657c9259b3aa57ad0eef52aa8f83c
Luka Tadic <luka.tadic365@gmail.com>
2023-01-26 15:16:26 +0100

    refactor: create executor module where command handling will be
    Move command handling into own separate module called executor. in main
    we should only initialise things and run main loop.

---
+++ src/executor/mod.rs
@@ -0,0 +1,179 @@
+mod command;
+mod state;
+
+pub use command::Command;
+use keepass::{Database, Entry, NodeRef, Value};
+use state::State;
+
+pub struct Executor {
+    state: State,
+}
+
+impl Executor {
+    pub fn new(db: Option<Database>) -> Self {
+        Self {
+            state: State::new(db),
+        }
+    }
+
+    pub fn get_current_group_name(&self) -> Option<&str> {
+        self.state
+            .db
+            .as_ref()
+            .map(|db| db.get_current_group().name.as_ref())
+    }
+
+    pub fn execute(&mut self, command: Command) {
+        let db = match &mut self.state.db {
+            Some(db) => db,
+            None => {
+                eprintln!("Database not opened!");
+                return;
+            }
+        };
+
+        match command {
+            Command::ListDir { path } => {
+                let group = db.get_current_group();
+                if let Some(node) = db.get_node(&group, &path) {
+                    list_node(node);
+                } else {
+                    eprintln!("{} does not exist!", path);
+                }
+            }
+            Command::ChangeDir { path } => match db.change_current_group(&path) {
+                false => {
+                    eprintln!("{} is not a group or doesn't exist!", path);
+                }
+                true => {}
+            },
+            Command::Show { show_hidden, entry } => {
+                let group = db.get_current_group();
+                if let Some(node) = db.get_node(&group, &entry) {
+                    print_node(node, show_hidden)
+                } else {
+                    eprintln!("{} does not exist!", entry);
+                }
+            }
+            Command::CopyPassword { entry } => {
+                let group = db.get_current_group();
+                match db.get_node(&group, &entry) {
+                    Some(NodeRef::Group(_)) | None => {
+                        eprintln!("{} is not a group or doesn't exist!", entry);
+                    }
+                    Some(NodeRef::Entry(e)) => copy_password(e),
+                }
+            }
+        }
+    }
+}
+
+/// This uses OSC52 terminal escape command
+/// which makes terminal emulator to copy data to system clipboard
+fn print_password_as_osc52(password: &[u8]) {
+    use base64::{engine::general_purpose, Engine as _};
+    let b64 = general_purpose::STANDARD.encode(password);
+    match std::env::var("TMUX") {
+        Ok(_) => {
+            print!("\x1bPtmux;\x1b\x1b]52;c;{}\x1b\x5c", b64);
+        }
+        Err(_) => {
+            print!("\x1b]52;c;{}", b64);
+        }
+    }
+    println!("Password copied to clipboard");
+}
+
+fn copy_password<'a>(entry: &'a Entry) {
+    match entry.fields.get("Password") {
+        Some(Value::Unprotected(pass)) => {
+            print_password_as_osc52(pass.as_bytes());
+        }
+        Some(Value::Protected(pass)) => {
+            print_password_as_osc52(pass.unsecure());
+        }
+        Some(Value::Bytes(pass)) => {
+            print_password_as_osc52(&pass);
+        }
+        None => {
+            eprintln!("Password is not set!");
+        }
+    }
+}
+
+fn list_node<'a>(node: NodeRef<'a>) {
+    match node {
+        NodeRef::Entry(e) => {
+            let title = e.get_title().unwrap_or("(no title");
+            println!("{}", title);
+        }
+        NodeRef::Group(g) => {
+            for node in &g.children {
+                match node.to_ref() {
+                    NodeRef::Group(g) => {
+                        println!("{}/", g.name);
+                    }
+                    NodeRef::Entry(e) => {
+                        let title = e.get_title().unwrap_or("(no title");
+                        println!("{}", title);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn print_node<'a>(node: NodeRef<'a>, show_hidden: bool) {
+    const FIELD_NAME_WIDTH: usize = 15;
+
+    fn get_value(val: &Value, show_hidden: bool) -> &str {
+        match val {
+            Value::Bytes(_) => "(bytes)",
+            Value::Unprotected(val) => &val,
+            Value::Protected(val) => {
+                if show_hidden {
+                    let val = std::str::from_utf8(val.unsecure()).unwrap_or("");
+                    val
+                } else {
+                    "*** SECRET ***"
+                }
+            }
+        }
+    }
+
+    match node {
+        NodeRef::Entry(e) => {
+            let title = e
+                .fields
+                .get("Title")
+                .map(|val| get_value(val, show_hidden))
+                .unwrap_or("(no title)");
+            let username = e
+                .fields
+                .get("UserName")
+                .map(|val| get_value(val, show_hidden))
+                .unwrap_or("(no username)");
+            let password = e
+                .fields
+                .get("Password")
+                .map(|val| get_value(val, show_hidden))
+                .unwrap_or("(no password)");
+            println!("{:>FIELD_NAME_WIDTH$}: {}", "Title", title);
+            println!("{:>FIELD_NAME_WIDTH$}: {}", "UserName", username);
+            println!("{:>FIELD_NAME_WIDTH$}: {}", "Password", password);
+
+            for (field_name, field_value) in &e.fields {
+                if field_name != "Title" && field_name != "UserName" && field_name != "Password" {
+                    println!(
+                        "{:>FIELD_NAME_WIDTH$}: {}",
+                        field_name,
+                        get_value(field_value, show_hidden),
+                    );
+                }
+            }
+        }
+        NodeRef::Group(_) => {
+            println!("");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,175 +1,13 @@
-mod command;
+mod executor;
 mod opt;
-mod state;
 
 use clap::Parser;
-use command::Command;
-use keepass::{Database, DatabaseOpenError, Entry, NodeRef, Value};
+use executor::{Command, Executor};
+use keepass::{Database, DatabaseOpenError};
 use opt::Opts;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-use state::State;
 use std::fs::File;
-
-/// This uses OSC52 terminal escape command
-/// which makes terminal emulator to copy data to system clipboard
-fn print_password_as_osc52(password: &[u8]) {
-    use base64::{engine::general_purpose, Engine as _};
-    let b64 = general_purpose::STANDARD.encode(password);
-    match std::env::var("TMUX") {
-        Ok(_) => {
-            print!("\x1bPtmux;\x1b\x1b]52;c;{}\x1b\x5c", b64);
-        }
-        Err(_) => {
-            print!("\x1b]52;c;{}", b64);
-        }
-    }
-    println!("Password copied to clipboard");
-}
-
-fn copy_password<'a>(entry: &'a Entry) {
-    match entry.fields.get("Password") {
-        Some(Value::Unprotected(pass)) => {
-            print_password_as_osc52(pass.as_bytes());
-        }
-        Some(Value::Protected(pass)) => {
-            print_password_as_osc52(pass.unsecure());
-        }
-        Some(Value::Bytes(pass)) => {
-            print_password_as_osc52(&pass);
-        }
-        None => {
-            eprintln!("Password is not set!");
-        }
-    }
-}
-
-fn list_node<'a>(node: NodeRef<'a>) {
-    match node {
-        NodeRef::Entry(e) => {
-            let title = e.get_title().unwrap_or("(no title");
-            println!("{}", title);
-        }
-        NodeRef::Group(g) => {
-            for node in &g.children {
-                match node.to_ref() {
-                    NodeRef::Group(g) => {
-                        println!("{}/", g.name);
-                    }
-                    NodeRef::Entry(e) => {
-                        let title = e.get_title().unwrap_or("(no title");
-                        println!("{}", title);
-                    }
-                }
-            }
-        }
-    }
-}
-
-fn print_node<'a>(node: NodeRef<'a>, show_hidden: bool) {
-    const FIELD_NAME_WIDTH: usize = 15;
-
-    fn get_value(val: &Value, show_hidden: bool) -> &str {
-        match val {
-            Value::Bytes(_) => "(bytes)",
-            Value::Unprotected(val) => &val,
-            Value::Protected(val) => {
-                if show_hidden {
-                    let val = std::str::from_utf8(val.unsecure()).unwrap_or("");
-                    val
-                } else {
-                    "*** SECRET ***"
-                }
-            }
-        }
-    }
-
-    match node {
-        NodeRef::Entry(e) => {
-            let title = e
-                .fields
-                .get("Title")
-                .map(|val| get_value(val, show_hidden))
-                .unwrap_or("(no title)");
-            let username = e
-                .fields
-                .get("UserName")
-                .map(|val| get_value(val, show_hidden))
-                .unwrap_or("(no username)");
-            let password = e
-                .fields
-                .get("Password")
-                .map(|val| get_value(val, show_hidden))
-                .unwrap_or("(no password)");
-            println!("{:>FIELD_NAME_WIDTH$}: {}", "Title", title);
-            println!("{:>FIELD_NAME_WIDTH$}: {}", "UserName", username);
-            println!("{:>FIELD_NAME_WIDTH$}: {}", "Password", password);
-
-            for (field_name, field_value) in &e.fields {
-                if field_name != "Title" && field_name != "UserName" && field_name != "Password" {
-                    println!(
-                        "{:>FIELD_NAME_WIDTH$}: {}",
-                        field_name,
-                        get_value(field_value, show_hidden),
-                    );
-                }
-            }
-        }
-        NodeRef::Group(_) => {
-            println!("");
-        }
-    }
-}
-
-fn handle_command<'a>(state: &'a mut State, command: &str) {
-    let db = match &mut state.db {
-        Some(db) => db,
-        None => {
-            eprintln!("Database not opened!");
-            return;
-        }
-    };
-    let command = match Command::try_parse(command) {
-        Err(err) => {
-            err.print();
-            return;
-        }
-        Ok(cmd) => cmd,
-    };
-    match command {
-        Command::ListDir { path } => {
-            let group = db.get_current_group();
-            if let Some(node) = db.get_node(&group, &path) {
-                list_node(node);
-            } else {
-                eprintln!("{} does not exist!", path);
-            }
-        }
-        Command::ChangeDir { path } => match db.change_current_group(&path) {
-            false => {
-                eprintln!("{} is not a group or doesn't exist!", path);
-            }
-            true => {}
-        },
-        Command::Show { show_hidden, entry } => {
-            let group = db.get_current_group();
-            if let Some(node) = db.get_node(&group, &entry) {
-                print_node(node, show_hidden)
-            } else {
-                eprintln!("{} does not exist!", entry);
-            }
-        }
-        Command::CopyPassword { entry } => {
-            let group = db.get_current_group();
-            match db.get_node(&group, &entry) {
-                Some(NodeRef::Group(_)) | None => {
-                    eprintln!("{} is not a group or doesn't exist!", entry);
-                }
-                Some(NodeRef::Entry(e)) => copy_password(e),
-            }
-        }
-    }
-}
 
 fn main() -> Result<(), DatabaseOpenError> {
     let opts = Opts::parse();
@@ -181,19 +19,26 @@ fn main() -> Result<(), DatabaseOpenError> {
         _ => None,
     };
 
-    let mut state = State::new(db);
+    let mut executor = Executor::new(db);
 
     let mut rl = Editor::<()>::new().unwrap();
     loop {
-        let readline = if let Some(db) = &state.db {
-            rl.readline(&format!("{}>> ", db.get_current_group().name))
+        let readline = if let Some(curr_group) = &executor.get_current_group_name() {
+            rl.readline(&format!("{}>> ", curr_group))
         } else {
             rl.readline(">> ")
         };
         match readline {
             Ok(line) => {
+                let command = match Command::try_parse(&line) {
+                    Err(err) => {
+                        err.print();
+                        continue;
+                    }
+                    Ok(cmd) => cmd,
+                };
                 rl.add_history_entry(line.as_str());
-                handle_command(&mut state, &line);
+                executor.execute(command);
             }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");

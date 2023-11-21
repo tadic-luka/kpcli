cbfcfb602336b90dc5e0b9baf099b867a79f5105
Luka Tadic <luka.tadic365@gmail.com>
2023-11-21 22:37:47 +0100

    chore: use keepass Group 'get' method for getting node from a path
    simplifies `get_node` method in Db struct

--- src/executor/state.rs
+++ src/executor/state.rs
@@ -64,32 +64,13 @@ impl Db {
         true
     }
 
-    // recursively try to get
     pub fn get_node<'a>(&'a self, group: &'a Group, path: &str) -> Option<NodeRef<'a>> {
-        fn get<'a>(group: &'a Group, path: &str) -> Option<NodeRef<'a>> {
-            match path {
-                "" | "./" | "." => Some(NodeRef::Group(group)),
-                _ => group.children.iter().find_map(|n| match n {
-                    Node::Group(g) if g.name == path => Some(n.as_ref()),
-                    Node::Entry(e) => {
-                        e.get_title()
-                            .and_then(|t| if t == path { Some(n.as_ref()) } else { None })
-                    }
-                    _ => None,
-                }),
+        match path {
+            "" | "./" | "." => Some(NodeRef::Group(group)),
+            _ => {
+                let full_path = path.split("/").collect::<Vec<_>>();
+                group.get(&full_path)
             }
         }
-
-        let mut node = group;
-        for path in path.split("/") {
-            match get(node, path) {
-                Some(NodeRef::Group(g)) => node = g,
-                Some(e @ NodeRef::Entry(_)) => return Some(e),
-                None => {
-                    return None;
-                }
-            }
-        }
-        Some(NodeRef::Group(node))
     }
 }
